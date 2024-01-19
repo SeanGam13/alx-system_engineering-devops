faed3c6e83dfa8848e31e789967ae054b71da50a
SeanGam13 <seangambanou9@gmail.com>
2024-01-18 17:12:38 -0800

    task 0 fail

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,27 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-        """
-    Returns the number of subscribers for a given subreddit
-
-    Args:
-        subreddit: the subreddit to be checked
-
-    Return:
-        0, if subreddit is invalid else the number or subscribers
-    """
-
-            if subreddit is None or not isinstance(subreddit, str):
-                        return None
-
-                        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-                            user_agent = "Google Chrome Version 81.0.4044.129"
-                                header = {"User-Agent": user_agent}
-                                    response = requests.get(url, headers=header)
-
-                                        try:
-                                                    data = response.json()
-                                                            subscribers = data.get('data').get('subscribers')
-                                                                    return (subscribers)
-                                        except Exception:
-                                                    return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
