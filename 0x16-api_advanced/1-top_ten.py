3a9d1d0417e54fa2925dbdc7e447b560ff77cfed
SeanGam13 <seangambanou9@gmail.com>
2024-01-18 17:16:38 -0800

    task 1 fail

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,18 +4,10 @@ import requests
 
 
 def top_ten(subreddit):
-"""Print the titles of the 10 hottest posts on a given subreddit."""
-url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-headers = {
-"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-}
-params = {
-"limit": 10
-}
-response = requests.get(url, headers=headers, params=params,
-			     allow_redirects=False)
-if response.status_code == 404:
-print("None")
-return
-results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """return total number of subscriber for a subreddit"""
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
+    headers = {'User-Agent': 'alx-api-12345/0.1 (by u/elghafiani)'}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    data = res.json()["data"]["children"]
+    for post in data[:10]:
+        print(post["data"]["title"])
